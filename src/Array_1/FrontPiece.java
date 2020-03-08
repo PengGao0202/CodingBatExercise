73bec4a80cda2ab5ef7f5a7b214b9fa4e89bf2db
PengGao0202 <concordia.ie@gmail.com>
2020-03-08 01:17:31 -0500

    14 methods added

---
+++ src/Array_1/FrontPiece.java
@@ -0,0 +1,12 @@
+package Array_1;
+
+public class FrontPiece {
+	public int[] frontPiece(int[] nums) {
+		if (nums.length < 2)
+			return nums;
+		int[] arr = new int[2];
+		arr[0] = nums[0];
+		arr[1] = nums[1];
+		return arr;
+	}
+}
---
+++ src/String_1/EndsLy.java
@@ -0,0 +1,11 @@
+package String_1;
+
+public class EndsLy {
+	public boolean endsLy(String str) {
+		if (str.length() < 2)
+			return false;
+		if (str.substring(str.length() - 2, str.length()).equals("ly"))
+			return true;
+		return false;
+	}
+}
